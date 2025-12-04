8566546dc0fca1952c6e195ebbe8cbdcdb369858
Ubuntu <ubuntu@ip-172-31-55-167.ec2.internal>
2025-12-04 05:04:32 +0000

    Modified the ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,10 @@
 resource "aws_ecr_repository" "blog_repo" {
   name = var.ecr_repo_name
   image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 	
   lifecycle_policy {
     policy = file("${path.module}/ecr_lifecycle_policy.json")
